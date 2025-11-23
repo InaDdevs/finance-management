0475354ab35cc53a18c2d1d5b2237c46871ad714
Dani Azevedo <dani.contatoxx@gmail.com>
2025-11-23 11:56:31 -0300

    Remocao da biblioteca

--- lib/features/auth/register_screen.dart
+++ lib/features/auth/register_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../../core/dart/providers/auth_provider.dart';
 
 class RegisterScreen extends StatefulWidget {
